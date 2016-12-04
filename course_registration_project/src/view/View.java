1c2d613e65f222dd8487cda6b68ede2b18a8cac1
Wallun Chan <walllun.chan@gmail.com>
2016-12-03 16:03:05 -0800

    Committing first major code checkin for course registration project. Contains working skeleton code that demos basic menu traversal and loading of course data.

---
+++ course_registration_project/src/view/View.java
@@ -0,0 +1,14 @@
+package view;
+
+import model.Model;
+import controller.Controller;
+
+public abstract class View {
+	protected Model model;
+	
+	protected Controller controller;
+	
+	public void display() {
+		
+	}
+}
